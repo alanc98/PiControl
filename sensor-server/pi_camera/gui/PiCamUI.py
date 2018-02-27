b0ae40b9556821a006a12a89c61fc1a757a65d13
alanc98 <Alan.Cudmore@gmail.com>
2018-02-27 18:31:46 -0500

    adding Pi Camera GUI functionality

--- sensor-server/pi_camera/gui/PiCamUI.py
+++ sensor-server/pi_camera/gui/PiCamUI.py
@@ -3,6 +3,7 @@
 #
 from PyQt4 import QtGui 
 import sys 
+import zmq
 
 import PiCamUIDesign 
               
@@ -27,8 +28,10 @@ class PiCamUIApp(QtGui.QMainWindow, PiCamUIDesign.Ui_MainWindow):
       self.videoPathLineEdit.setText("/home/Pi/Pictures/picam_video001.h264")
       self.timelapsePathLineEdit.setText("/home/Pi/Pictures/timelapse-01-")
 
-      # Pre-populate IP address
+      # Pre-populate IP address and ports
       self.ipAddressLineEdit.setText("127.0.0.1")
+      self.reqPortLineEdit.setText("5557")
+      self.subPortLineEdit.setText("5558")
 
       # Pre-populate Camera Status
       self.cameraStatusLineEdit.setText("IDLE")
@@ -37,68 +40,95 @@ class PiCamUIApp(QtGui.QMainWindow, PiCamUIDesign.Ui_MainWindow):
       self.imagePushButton.clicked.connect(self.send_capture_image_message) 
       self.videoPushButton.clicked.connect(self.send_capture_video_message) 
       self.timelapsePushButton.clicked.connect(self.send_capture_timelapse_message) 
+      self.ConnectPushButton.clicked.connect(self.connect_to_ports) 
+      self.connected = False
 
+      # setup ZMQ context and create socket
+      self.context = zmq.Context()
+      self.req_socket = self.context.socket(zmq.REQ)
+      self.req_socket_connected = False
+
+   def send_camera_sensor_req(self, req_message, ip_addr):
+       print 'Sending Message to Address:' + ip_addr
+       print req_message
+       ip_string = 'tcp://' + ip_addr + ':5557'
+       if self.req_socket_connected == False:
+           self.req_socket.connect(str(ip_string))
+       self.req_socket.send(str(req_message))
+       reply_message = self.req_socket.recv()
+       print 'Received Pi_Camera sensor reply:' , reply_message
+
+   def connect_to_ports(self):
+       if self.connected == False:
+          print 'connecting to the ZMQ remote ports!'
+          self.connected = True
+          self.connectedCheckBox.setChecked(True)
+       else:
+           print 'already connected to the ports!'
 
    def send_capture_image_message(self):
-      print 'Send a capture image message!'                       
       image_size_text = self.imageSizeComboBox.currentText()
-      print '  size --> ',image_size_text
-      image_file_name = self.imagePathLineEdit.text()
-      print '  file --> ',image_file_name
-      if self.imageCheckBox.isChecked() == True:
-          flip_image = True
-          print '  Flip image '
+      if image_size_text == '1024x768':
+          message_size_text = '1'
+      elif image_size_text == '1920x1080':
+          message_size_text = '2'
       else:
-          flip_image = False
-          print '  Do not flip image'
+          message_size_text = '3'
+      image_file_name = self.imagePathLineEdit.text()
+      if self.imageCheckBox.isChecked() == True:
+          flip_text = 'TRUE'
+      else:
+          flip_text = 'FALSE'
+      sensor_req_message = 'SENSOR_REQ,DEV=PI_CAMERA,SUB_DEV=STILL,CMD=CAPTURE,SIZE=' + message_size_text + ',VFLIP=' + flip_text + ',FILE=' + image_file_name + ',SENSOR_REQ_END'
       ip_address = self.ipAddressLineEdit.text()
-      print '  send message to : ',ip_address
-
+      self.send_camera_sensor_req(sensor_req_message, ip_address)
 
    def send_capture_video_message(self):
-      print 'Send a capture video message!'                       
       video_size_text = self.videoSizeComboBox.currentText()
-      print '  size --> ',video_size_text
-      video_file_name = self.videoPathLineEdit.text()
-      print '  file --> ',video_file_name
-      if self.videoCheckBox.isChecked() == True:
-          flip_video = True
-          print '  Flip Video '
+      if video_size_text == '640x480':
+          size_text = '1'
+      elif video_size_text == '1280x720':
+          size_text = '2'
       else:
-          flip_video = False
-          print '  Do not flip Video'
+          size_text = '3'
+      video_file_name = self.videoPathLineEdit.text()
+      if self.videoCheckBox.isChecked() == True:
+          flip_text = 'TRUE'
+      else:
+          flip_text = 'FALSE'
       video_seconds = self.videoLengthSpinBox.value()
-      print '  video length in seconds --> ',video_seconds
+      duration_text = str(video_seconds)
 
+      sensor_req_message = 'SENSOR_REQ,DEV=PI_CAMERA,SUB_DEV=VIDEO,CMD=CAPTURE,SIZE=' + size_text + ',VFLIP=' + flip_text + ',DURATION=' + duration_text + ',FILE=' + video_file_name + ',SENSOR_REQ_END'
       ip_address = self.ipAddressLineEdit.text()
-      print '  send message to : ',ip_address
+      self.send_camera_sensor_req(sensor_req_message, ip_address)
 
  
    def send_capture_timelapse_message(self):
-      print 'Send a capture timelapse message!'                       
       image_size_text = self.timelapseSizeComboBox.currentText()
-      print '  size --> ',image_size_text
-      image_file_prefix = self.timelapsePathLineEdit.text()
-      print '  file prefix --> ', image_file_prefix
-      if self.timelapseCheckBox.isChecked() == True:
-          flip_image = True
-          print '  Flip image '
+      if image_size_text == '1024x768':
+          message_size_text = '1'
+      elif image_size_text == '1920x1080':
+          message_size_text = '2'
       else:
-          flip_image = False
-          print '  Do not flip image'
-      number_of_frames = self.timelapseFramesSpinBox.value()
-      print '  number of frames --> ', number_of_frames
-      frame_delay = self.timelapseDelaySpinBox.value()
-      print '  delay between frames --> ', frame_delay
+          message_size_text = '3'
+      image_file_prefix = self.timelapsePathLineEdit.text()
+      if self.timelapseCheckBox.isChecked() == True:
+          flip_text = 'TRUE'
+      else:
+          flip_text = 'FALSE'
+      num_frames_text = str(self.timelapseFramesSpinBox.value())
+      frame_delay_text = str(self.timelapseDelaySpinBox.value())
+      sensor_req_message = 'SENSOR_REQ,DEV=PI_CAMERA,SUB_DEV=TIMELAPSE,CMD=CAPTURE,SIZE=' + message_size_text + ',VFLIP=' + flip_text + ',FILE_PRE=' + image_file_prefix + ',DELAY=' + frame_delay_text + ',FRAMES=' + num_frames_text + ',SENSOR_REQ_END'
       ip_address = self.ipAddressLineEdit.text()
-      print '  send message to : ',ip_address
+      self.send_camera_sensor_req(sensor_req_message, ip_address)
 
 
 def main():
-   app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
-   form = PiCamUIApp()                 # We set the form to be our ExampleApp (design)
-   form.show()                         # Show the form
-   app.exec_()                         # and execute the app
+   app = QtGui.QApplication(sys.argv) 
+   form = PiCamUIApp() 
+   form.show() 
+   app.exec_() 
 
-if __name__ == '__main__':             # if we're running file directly and not importing it
-   main()                              # run the main function
+if __name__ == '__main__': 
+   main()        
